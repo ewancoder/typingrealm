e3d8af90d44844e2c338ddae586fe65ca068c68d
Ivan Zyranau <ewancoder@gmail.com>
2024-02-06 10:04:52 +0300

    Add diagnostics controller to show information about service instance

--- TypingRealm.ApiHost/ApiHostStartupFilter.cs
+++ TypingRealm.ApiHost/ApiHostStartupFilter.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using System.Text.Json;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace TypingRealm.ApiHost;
+
+public sealed record DiagnosticsData(
+    string Instance,
+    DateTime Now,
+    int Count);
 
 public sealed class ApiHostStartupFilter : IStartupFilter
 {
@@ -18,7 +25,17 @@ public sealed class ApiHostStartupFilter : IStartupFilter
             app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
+                var count = 0;
+                var instance = Guid.NewGuid().ToString();
                 endpoints.MapControllers();
+                endpoints.MapGet("/diag", async context =>
+                {
+                    var content = JsonSerializer.Serialize(
+                        new DiagnosticsData(instance, DateTime.Now, count++));
+
+                    await context.Response.WriteAsync(content)
+                        .ConfigureAwait(false);
+                });
             });
 
             next(app);
