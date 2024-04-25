9d4f549d88d0c502f4988d04246d0b4ee78a54d4
Ivan Zyranau <ewancoder@gmail.com>
2024-04-25 15:16:42 +0400

    Add minimal statistics (speed, errors)

--- Backend/TypingRealm.Typing.Api/Controllers/TypingController.cs
+++ Backend/TypingRealm.Typing.Api/Controllers/TypingController.cs
@@ -1,11 +1,18 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TypingRealm.Typing.DataAccess;
 
 namespace TypingRealm.Typing.Api.Controllers;
+
+public sealed record TypingStatistics(
+    decimal minSpeed,
+    decimal maxSpeed,
+    decimal averageSpeed,
+    IEnumerable<KeyValuePair<string, string[]>> mistakes);
 
 [Authorize]
 [Route("api/typing")]
@@ -16,6 +23,92 @@ public sealed class TypingController : ControllerBase
     public TypingController(ITypingRepository typingRepository)
     {
         _typingRepository = typingRepository;
+    }
+
+    // This will be refactored later so that the statistics is re-calculating every time new result is pushed, and there's no need to re-iterate over all user texts.
+    [HttpGet]
+    [Route("statistics")]
+    public async ValueTask<TypingStatistics> CalculateTypingStatistics()
+    {
+        var errors = new List<string>();
+        var totalCharacters = 0m;
+        var totalTime = 0m;
+        var minSpeed = 0m;
+        var maxSpeed = 0m;
+
+        foreach (var info in await _typingRepository.GetAllTypingSessionInfosAsync())
+        {
+            var typing = await _typingRepository.GetTypingResultByIdAsync(info.Id)
+                ?? throw new InvalidOperationException("Could not load typing result.");
+
+            var text = typing.Text;
+            var index = 0;
+            var typed = string.Empty;
+            var errorIndex = 0;
+
+            decimal firstPerf = 0;
+            decimal lastPerf = 0;
+            foreach (var e in typing.Events)
+            {
+                if (e.KeyAction == KeyAction.Release)
+                    continue;
+
+                if (firstPerf == 0)
+                    firstPerf = e.Perf;
+                lastPerf = e.Perf;
+
+                if (e.Key.ToLowerInvariant() == "backspace" && index > 0)
+                {
+                    if (errorIndex == index)
+                        errorIndex = 0;
+
+                    index--;
+                    typed = typed[..^1];
+                    continue;
+                }
+
+                if (e.Key.Length != 1)
+                    continue;
+
+                if (e.Key[0] == text[index])
+                {
+                    typed += e.Key;
+                    index++;
+                    continue;
+                }
+
+                // If we got here - there's an error.
+                typed += e.Key;
+                index++;
+
+                if (errorIndex == 0)
+                {
+                    errorIndex = index;
+                    if (typed.Length > 1)
+                        errors.Add($"{typed[typed.Length - 2]}{text[index - 1]}");
+                    else
+                        errors.Add(text[0].ToString());
+                }
+            }
+
+            var speed = text.Length / ((lastPerf - firstPerf) / 60000m);
+            minSpeed = minSpeed == 0 ? speed : Math.Min(minSpeed, speed);
+            maxSpeed = Math.Max(maxSpeed, speed);
+            totalCharacters += text.Length;
+            totalTime += lastPerf - firstPerf;
+
+            if (typed != text)
+                throw new InvalidOperationException("Could not successfully parse typing session to the end.");
+        }
+
+        var pairs = errors.GroupBy(x => x.Length == 1 ? x : x[1..])
+            .OrderByDescending(x => x.Count())
+            .Select(x => new KeyValuePair<string, string[]>(x.Key, x.ToArray()))
+            .ToList();
+
+        var averageSpeed = totalCharacters / (totalTime / 60000m);
+
+        return new TypingStatistics(minSpeed, maxSpeed, averageSpeed, pairs);
     }
 
     [HttpGet]
