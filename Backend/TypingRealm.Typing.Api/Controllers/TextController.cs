409999ab0e254fdf35dd932f3feef9b38128a51e
Ivan Zyranau <ewancoder@gmail.com>
2024-04-25 04:05:35 +0400

    Add OpenAI key secret & Text generation API

---
+++ Backend/TypingRealm.Typing.Api/Controllers/TextController.cs
@@ -0,0 +1,50 @@
+﻿using Azure.AI.OpenAI;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace TypingRealm.Typing.Api.Controllers;
+
+// This controller will be moved to a separate project/service later, together with OpenAI functionality.
+[Authorize]
+[Route("api/text")]
+public sealed class TextController : ControllerBase
+{
+    private const int MaxTextLength = 1000;
+    private readonly OpenAIClient _openAiClient;
+
+    public TextController(IConfiguration configuration)
+    {
+        var openAiKey = configuration["OpenAIKey"]
+            ?? throw new InvalidOperationException("Could not read OpenAIKey.");
+
+        _openAiClient = new OpenAIClient(openAiKey);
+    }
+
+    [HttpGet]
+    [Route("generate")]
+    public async ValueTask<ActionResult<string>> GenerateText(int length)
+    {
+        if (length <= 10)
+            return BadRequest($"Length should be at least 10 characters long.");
+
+        if (length > MaxTextLength)
+            return BadRequest($"Length should not exceed {MaxTextLength} characters.");
+
+        var options = new ChatCompletionsOptions
+        {
+            DeploymentName = "gpt-3.5-turbo",
+            Messages =
+            {
+                new ChatRequestSystemMessage("Generate text without any ambient info"),
+                new ChatRequestUserMessage($"Generate meaningful thematic text {length} characters long, to train typing across whole keyboard decently spread out")
+            }
+        };
+
+        var response = await _openAiClient.GetChatCompletionsAsync(options);
+        var responseMessage = response.Value.Choices[0].Message;
+        return responseMessage.Content;
+    }
+}
--- README.md
+++ README.md
@@ -2,6 +2,28 @@
 
 This is a analytics and training tool to gather statistics and improve your typing. In future, more gaming elements will be added to it until it's a full-fledged game. But the first iteration of this project is simple: gather statistics on typing for future learning and improvement.
 
+## Prerequisites
+
+**IMPORTANT**
+
+For the text generation feature to work, you need a working OpenAI API key configured in you secret manager (for local development) or environment variable (for deployment).
+
+For local development, do this in the `TypingRealm.Typing.Api` project folder:
+
+```
+dotnet user-secrets set "OpenAIKey" "xxx"
+```
+
+where `xxx` is your personal OpenAI access key.
+
+For deployment, you need to specify the `OpenAIKey` environment variable in a special `secrets.env` file in the root of the repository, that is being used by docker-compose. It is not checked in to the repository so you need to create it with the following content:
+
+```
+OpenAIKey=xxx
+```
+
+where `xxx` is your personal OpenAI access key.
+
 ## Deploying the project locally
 
 Just run in the root folder
