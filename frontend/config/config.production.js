352e25f8a07d703158130b9df4835a3d0a77d45d
Ivan Zyranau <ewancoder@gmail.com>
2024-10-08 04:09:20 +0400

    Edit files to be configured for production by default
    Come up with a better way (for local debugging).

--- frontend/config/config.production.js
+++ frontend/config/config.production.js
@@ -1,8 +1,8 @@
 export const config = {
-    //typingApiUrl: 'https://api.typingrealm.com/typing/api/typing'
-    //textApiUrl: 'https://api.typingrealm.com/typing/api/text',
-    //statisticsApiUrl: 'https://api.typingrealm.com/typing/api/statistics'
-    typingApiUrl: 'https://api.localhost/typing/api/typing',
-    textApiUrl: 'https://api.localhost/typing/api/text',
-    statisticsApiUrl: 'https://api.localhost/typing/api/statistics'
+    typingApiUrl: 'https://api.typingrealm.com/typing/api/typing',
+    textApiUrl: 'https://api.typingrealm.com/typing/api/text',
+    statisticsApiUrl: 'https://api.typingrealm.com/typing/api/statistics'
+    //typingApiUrl: 'https://api.localhost/typing/api/typing',
+    //textApiUrl: 'https://api.localhost/typing/api/text',
+    //statisticsApiUrl: 'https://api.localhost/typing/api/statistics'
 }
