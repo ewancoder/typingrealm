8ce074c558ed886806bf5ad23dae57bb4c8e4555
Ivan Zyranau <ewancoder@gmail.com>
2024-10-08 17:11:26 +0400

    Add development dockerfile for web UI

---
+++ frontend/config/config.development.js
@@ -0,0 +1,5 @@
+export const config = {
+    typingApiUrl: 'https://dev.api.typingrealm.com/typing/api/typing',
+    textApiUrl: 'https://dev.api.typingrealm.com/typing/api/text',
+    statisticsApiUrl: 'https://dev.api.typingrealm.com/typing/api/statistics'
+}
