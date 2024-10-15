0e02ae328e486ae436625ebdc0c9db7cf74da809
Ivan Zyranau <ewancoder@gmail.com>
2024-10-15 12:09:53 +0400

    Add listing all locations to editor template

--- frontend-game/src/app/editor/locations/locations-editor/locations-editor.component.ts
+++ frontend-game/src/app/editor/locations/locations-editor/locations-editor.component.ts
@@ -1,23 +1,27 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { LocationService } from '../location.service';
+import { Observable } from 'rxjs';
+import { Location, LocationService } from '../location.service';
 
 @Component({
     selector: 'tyr-locations-editor',
     standalone: true,
-    imports: [ReactiveFormsModule],
+    imports: [ReactiveFormsModule, AsyncPipe],
     templateUrl: './locations-editor.component.html',
     styleUrl: './locations-editor.component.scss',
-    encapsulation: ViewEncapsulation.None
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LocationsEditorComponent {
+    locations$!: Observable<Location[]>;
     newLocationForm = new FormGroup({
         name: new FormControl('', Validators.required),
         description: new FormControl('', Validators.required)
     });
 
     constructor(private locationService: LocationService) {
-        locationService.getLocations$().subscribe(x => console.log(x));
+        this.locations$ = locationService.getLocations$();
     }
 
     onSubmit() {
